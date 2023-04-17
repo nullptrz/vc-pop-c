ac56c9d895536dad6b5594418a36f4eba2aab266
Ali Ahsan Saeed <ahusunxy.coding@gmail.com>
2023-04-17 17:39:01 +0000

    arrays added

---
+++ iteration.c
@@ -0,0 +1,13 @@
+// for loop
+// while
+// do while
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+            printf("%d", i);
+    }
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    double total;
+    scanf("%lf", &total);
+    printf("%lf", total);
+}
